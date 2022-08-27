3a93ec178588c857d8247e42aafda1518e9a518b
shazaib491 <shahzaib.rehman@neosoftmail.com>
2022-08-27 16:11:03 +0530

    latest

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const express = require("express");
-//calling top level function
 require("./utils/db");
 const blogsRoutes = require("./routes/blogs.routes");
 const app = express();
-
-
-// initialise port no
 const PORT = 3000;
-// middle ware function
-
-app.use(express.json());//converting response to string
-
+app.use(express.json());
 app.use(blogsRoutes);
-
-// to create server in nodejs
 app.listen(PORT, () => {
     console.log("Server is listening at port no", PORT);
 })
