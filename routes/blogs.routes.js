1c3937f34e320cb1f5ceeb7b3e38ea4d2a1516ab
shazaib491 <shahzaib.rehman@neosoftmail.com>
2022-08-27 16:26:00 +0530

    added exprots

--- routes/blogs.routes.js
+++ routes/blogs.routes.js
@@ -13,6 +13,14 @@ router.patch("/updateData/:id", blogsController.updateData)
 
 router.delete("/deleteData/:id", blogsController.deleteData)
 
+router.post("exports", () => {
+    console.log("shahzaib exports");
+})
+
+router.get("/saveer", () => {
+    console.log("I am from protectr")
+})
+
 router.all("/protetctir", (req, res, next) => {
     res.json({ message: "Hello i am protector'" })
 })
